cfca75ca0358f3c78ae8892768d256d4a959d1ac
adityahas <adityahas@gmail.com>
2018-09-27 23:18:17 +0700

    Fixed shaking camera

--- scenes/gameplay/Camera2D.gd
+++ scenes/gameplay/Camera2D.gd
@@ -3,10 +3,10 @@ extends Camera2D
 onready var mc = Utils.get_main_node().get_node("MainCharacter")
 
 func _ready():
-	set_process(true)
+	set_physics_process(true)
 	pass
 	
-func _process(delta):
+func _physics_process(delta):
 	position = Vector2(mc.position.x, position.y)
 	pass
 	
--- scenes/gameplay/MainCharacter.gd
+++ scenes/gameplay/MainCharacter.gd
@@ -1,7 +1,13 @@
 extends KinematicBody2D
 
+const GRAVITY_VEC = Vector2(0, 900)
+const FLOOR_NORMAL = Vector2(0, -1)
+const SLOPE_SLIDE_STOP = 25.0
+const WALK_SPEED = 160 # pixels/sec
+const JUMP_SPEED = 480
+
 var timer = 0.0
-const MOTION_SPEED = 160 # Pixels/second
+var linear_vel = Vector2()
 
 func _ready():
 	set_process_input(true)
@@ -10,24 +16,36 @@ func _ready():
 	pass
 	
 func _physics_process(delta):
-	Game.travel_distance = position.x
-	var motion = Vector2()
-
 	if Game.game_is_running():
-		motion = Vector2(1, 0)
-		motion = motion.normalized() * (MOTION_SPEED + Game.walk_speed)
-		move_and_slide(motion)
+		Game.travel_distance = position.x
+
+		# Apply Gravity
+		linear_vel += delta * GRAVITY_VEC
+		
+		var target_speed = 1
+		target_speed *= WALK_SPEED + Game.walk_speed
+		linear_vel.x = lerp(linear_vel.x, target_speed, 0.1)
+		
+		# Move and Slide
+		linear_vel = move_and_slide(linear_vel, FLOOR_NORMAL, SLOPE_SLIDE_STOP)
+		
+		if linear_vel.y == 0:
+			get_node("AnimatedSprite").play("run")
+		
 		timer += delta
 		if timer > 4:
 			timer = 0
 			Game.walk_speed += 30
-			get_node("AnimatedSprite").frames.set_animation_speed("run", get_node("AnimatedSprite").frames.get_animation_speed("run") + 1)
+			# Set run animation FPS
+			get_node("AnimatedSprite").frames.set_animation_speed("run", get_node("AnimatedSprite").frames.get_animation_speed("run") + 1)	
+	pass
 	
 func jump():
-#	linear_velocity.y -= 500
+	linear_vel.y = -JUMP_SPEED
+	get_node("AnimatedSprite").play("jump")
 	pass
 	
 func _input(event):
-#	if event.is_action_pressed("jump") && linear_velocity.y == 0 && Game.game_is_running():
-#		jump()
+	if event.is_action_pressed("jump") && is_on_floor() && Game.game_is_running():
+		jump()
 	pass
